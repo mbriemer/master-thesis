Fix NN training and loss calculation
- Implement multiple minimization in estimation.py (as in original code)
- Fix NN training and calculation of generator loss
- Plot correct NN loss plots

Code/sp/NND_sp.py
@@ -1,6 +1,7 @@
 from sklearn.neural_network import MLPClassifier
 import numpy as np
 #import logging
+from roy import royinv
 
 #logging.basicConfig(level=logging.INFO)
 #logger = logging.getLogger(__name__)
@@ -57,18 +58,20 @@ def average_discriminators(models, fake_samples):
 def NND_train(true_samples, fake_samples, num_hidden=10):
     #print("Training NND")
     #Set random seed of NN training
-    np.random.seed(456)
-    input_data = np.concatenate((true_samples, fake_samples))
+    input_data = np.column_stack((true_samples, fake_samples)).T
     nplusm = len(input_data)
-    labels = np.concatenate((np.ones(len(true_samples)), np.zeros(len(fake_samples))))
+    labels = np.column_stack((np.ones_like(true_samples), np.zeros_like(fake_samples)))[0,:].T
     model = MLPClassifier(hidden_layer_sizes=(num_hidden,),
                           activation='tanh', #paper
-                          solver='lbfgs',
+                          solver='adam',
                           alpha=0.01, #net.performParam.regularization = 0.01; cannot be easily translated
                           #batch_size=nplusm, #not needed with lbfgs
                           max_iter=2000)#, learning_rate_init=0.001, early_stopping=True, validation_fraction=0.1)
     model.fit(input_data, labels)
-    #print("NND trained")   
+    if not (model.classes_[0] == 0 and model.classes_[1] == 1):
+        print("Warning: classes are not [0, 1], switching them")
+        model.classes_[:, [0, 1]] = model.classes_[:, [1, 0]]
+    #print("NND trained")
     '''
     # Check convergence based on loss improvement
     if hasattr(model, 'loss_curve_'):
@@ -83,12 +86,24 @@ def NND_train(true_samples, fake_samples, num_hidden=10):
 
 def generator_loss(true_samples, fake_samples, num_hidden=10, num_models=1):
     models = [NND_train(true_samples, fake_samples, num_hidden) for _ in range(num_models)]
-    #avg_model = average_discriminators(models, fake_samples)
-    #print(f"Average model predictions: {avg_model.predict_proba(fake_samples)}")
-    average_prediction_fake = np.mean([model.predict_proba(fake_samples) for model in models], axis=0)
-    average_prediction_true = np.mean([model.predict_proba(true_samples) for model in models], axis=0)
+    fakearray = np.asarray(fake_samples).T
+    truearray = np.asarray(true_samples).T
+    average_prediction_fake = np.mean([model.predict_proba(fakearray)[:,1] for model in models], axis=0)
+    average_prediction_true = np.mean([model.predict_proba(truearray)[:,1] for model in models], axis=0)
     generator_loss = np.mean(np.log(average_prediction_true)) + np.mean(np.log(1 - average_prediction_fake))
     #discriminator_loss = np.mean(np.log(avg_model.predict_proba(true_samples))) + np.mean(np.log(1 - avg_model.predict_proba(fake_samples)))
     #print(f"Discriminator loss: {discriminator_loss}")
     #print(f"Generator loss: {generator_loss}")
     return generator_loss
+
+"""
+# Test
+
+u_1 = np.random.rand(100, 4)
+u_2 = np.random.rand(100, 4)
+theta_1 = np.array([1.8, 2, 0.5, 0, 1, 1, 0.5])
+theta_2 = np.array([1.9, 2.1, 0.6, 0.1, 1.1, 1.1, 0.6])
+X_1 = royinv(u_1, theta_1)
+X_2 = royinv(u_2, theta_2)
+print(generator_loss(X_1, X_2, num_models=1))
+"""

Code/sp/estimation.py
@@ -4,36 +4,33 @@ import numpy as np
 import scipy.optimize as opt
 import multiprocessing as mp
 
-from roy import royinv
+from roy import royinv, logroypdf
 from NND_sp import generator_loss
-from other_discriminators import OracleD
+from other_discriminators import OracleD, logistic_loss
 
 def callback(xk):
-    print(f"Current theta: {xk}")
+    pass
+    #print(f"Current theta: {xk}")
 
 def quadratic_loss(true_theta, theta):
     noise = 0.01 * np.random.randn(7)
     return np.sum(((true_theta - theta) + noise)**2)
 
-def oracle_loss(true_values, u, true_theta, theta):
+def loss_function(theta, u, true_values, loss, loss_options):
     fake_values = royinv(u, theta)
-    return OracleD(true_values, fake_values, true_theta, theta)
 
-def loss_function(theta, u, true_values, num_hidden, g):
-    #try:
-    fake_values = royinv(u, theta)
-    #    if fake_values is None:
-    #        print(f"royinv returned None for theta: {theta}")
-    #         return np.inf
-    loss = generator_loss(true_values, fake_values, num_hidden=num_hidden, num_models=g)
-    #    if loss is None:
-    #        print(f"generator_loss returned None for theta: {theta}")
-    #        return np.inf
-    #print(f"Loss for theta {theta}: {loss}")
-    return -loss
-    #except Exception as e:
-    #    print(f"Error in loss_function for theta {theta}: {str(e)}")
-    #    return np.inf
+    if loss == 'MLE':
+        return - np.sum(logroypdf(true_values, theta))
+    
+    elif loss == 'oracle':
+        return OracleD(true_values, fake_values, loss_options['true_theta'], theta)
+
+    elif loss == 'logistic':
+        loss, _ = logistic_loss(true_values, fake_values)
+        return loss
+    
+    elif loss == 'NN':
+        return generator_loss(true_values, fake_values, num_hidden=loss_options['num_hidden'], num_models=loss_options['num_models'])
     
 def run_single_repetition(args):
     rep, seed, generator_function, true_theta, num_hidden, g, num_samples = args
@@ -59,19 +56,67 @@ def run_single_repetition(args):
         upper_bounds = [3, 3, 1.5, 1, 2, 2, 1, 0.99]#, 0.9]
         sp_bounds = list(zip(lower_bounds, upper_bounds))
 
-        result = opt.minimize(
-            fun=lambda theta: oracle_loss(true_values, u, true_theta, theta), #loss_function(theta, u, true_values, num_hidden, g), #quadratic_loss(true_theta, theta),#
+        # MLE
+        
+        result_MLE = opt.minimize(
+            fun=lambda theta: loss_function(theta, u, true_values, 'MLE', None),
             x0=theta_initial_guess,
             method='Nelder-Mead',
             bounds=sp_bounds,
             callback=callback,
-            options={'disp': True, 'return_all': True, 'adaptive': True, 'maxiter': 600}
+            options={'disp': True, 'return_all': True, 'adaptive': True}#, 'maxiter': 600}
+        )
+
+        if not result_MLE.success:
+            print(f"Optimization failed for repetition {rep} with MLE: {result_MLE.message}")
+
+        # Adversarial estimator with the oracle discriminator
+        print(f"Starting oracle in repetition {rep}")
+        result_oracle = opt.minimize(
+            fun=lambda theta: loss_function(theta, u, true_values, 'oracle', {'true_theta': true_theta}),
+            x0=result_MLE.x,
+            method='Nelder-Mead',
+            bounds=sp_bounds,
+            callback=callback,
+            options={'disp': True, 'return_all': True, 'adaptive': True}#, 'maxiter': }
+        )
+
+        if not result_oracle.success:
+            print(f"Optimization failed for repetition {rep} with oracle discriminator: {result_oracle.message}")
+
+        # Adversarial estimator with a logistic discriminator
+        
+        print(f"Starting logistic in repetition {rep}")
+        result_logistic = opt.minimize(
+            fun=lambda theta: loss_function(theta, u, true_values, 'logistic', None),
+            x0=result_oracle.x,
+            method='Nelder-Mead',
+            bounds=sp_bounds,
+            callback=callback,
+            options={'disp': True, 'return_all': True, 'adaptive': True}#, 'maxiter': maxiter}
         )
         
-        if not result.success:
-            print(f"Optimization failed for repetition {rep}: {result.message}")
+
+        if not result_logistic.success:
+            print(f"Optimization failed for repetition {rep} with logistic discriminator: {result_logistic.message}")
         
-        return rep, theta_initial_guess, result, None
+        
+        # Adversarial estimator with a neural network discriminator
+        print(f"Starting NN in repetition {rep}")
+        result_NN = opt.minimize(
+            fun=lambda theta: loss_function(theta, u, true_values, 'NN', {'num_hidden': num_hidden, 'num_models': g}),
+            x0=theta_initial_guess,
+            method='Nelder-Mead',
+            bounds=sp_bounds,
+            callback=callback,
+            options={'disp': True, 'return_all': True, 'adaptive': True, 'maxiter': 100}
+        )
+        
+        if not result_NN.success:
+            print(f"Optimization failed for repetition {rep} with NN discriminator: {result_NN.message}")
+        
+        #result = {result_MLE, result_oracle, result_logistic, result_NN}
+        return rep, theta_initial_guess, result_NN, None
     except Exception as e:
         print(f"Error in repetition {rep}: {str(e)}")
         return rep, None, None, str(e)

Code/sp/loss_plots.py
@@ -48,8 +48,8 @@ LL_grid = np.zeros_like(param_grid)
 # Assume these variables are defined elsewhere
 theta = [1.8, 2, 0.5, 0, 1, 1, 0.5]
 #theta2: np.ndarray
-n = 3000
-m = 3000
+n = 300
+m = 300
 lambda_ = 0
 g = 30
 
@@ -73,7 +73,7 @@ for i in tqdm(range(len(theta))):
         th = theta.copy()
         th[i] = param_grid[i, k]
         #cD_grid[i, k] = loss(X, royinv(Z, th, smooth=lambda_))
-        #NND_grid[i, k] = NND(X, royinv(Z, th, lambda_, n), num_hidden=10, num_models=g)
+        NND_grid[i, k] = NND(X, royinv(Z, th), num_hidden=10, num_models=g)
         OracleD_grid[i, k] = OracleD(X, royinv(Z, th), theta, th)
         LL_grid[i, k] = -np.mean(logroypdf(X, th)) / 2
         #LL_grid_finite[i, k] = np.minimum(LL_grid[i, k], -1)
@@ -91,7 +91,7 @@ for i in range(len(theta)):
     #max_diff = np.absolute(OracleD_grid[i, :].max() - LL_grid[i, np.isfinite(LL_grid[i,:])].max())
     
     # Plot all curves on the same y-axis
-    #ax.plot(param_grid[i, :], NND_grid[i, :], linewidth=1.5, color='blue', label='$\\mathbf{M}_\\theta(\\hat{D}_\\theta)$')
+    ax.plot(param_grid[i, :], NND_grid[i, :], linewidth=1.5, color='blue', label='$\\mathbf{M}_\\theta(\\hat{D}_\\theta)$')
     ax.plot(param_grid[i, :], OracleD_grid[i, :], linewidth=1.5, color='green', label='$\\mathbf{M}_\\theta(D_\\theta)$')
     axL.plot(param_grid[i, :], (LL_grid[i, :] / 2), linewidth=1.5, color='red', label='$\\mathbf{L}_\\theta$')
     
@@ -105,8 +105,10 @@ for i in range(len(theta)):
     
     # Set axes limits
     ax.set_xlim(param_grid[i, 0], param_grid[i, -1])
-    #ax.set_ylim(-1.4, -1.25)
-    ax.set_ylim(OracleD_grid[i, :].min(), OracleD_grid[i, :].max())
+    ax.set_ylim(-1.4, -1.25)
+    #y_min = np.minimum(NND_grid[i, :].min(), OracleD_grid[i, :].min())
+    #y_max = np.maximum(NND_grid[i, :].max(), OracleD_grid[i, :].max())
+    #ax.set_ylim(y_min, y_max)
     axL.set_ylim((LL_grid[i, :].min())/2 - 0.01, (LL_grid[i, np.isfinite(LL_grid[i,:])].max())/2 + 0.01)
     #axL.set_ylim(0.2, 0)
     #y_max = np.maximum(OracleD_grid[i, :].max(), (LL_grid[i, np.isfinite(LL_grid[i,:])].max() - max_diff))

Code/sp/main_roy.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     lower_bounds = np.array([1, 1, -.5, -1, 0, 0, -1])#, 0, 0.9])
     upper_bounds = np.array([3, 3, 1.5, 1, 2, 2, 1])#, 0, 0.9])
 
-    num_samples = 3000
+    num_samples = 300
     num_repetitions = 10
     #n_discriminator = 100
 

Code/sp/other_discriminators.py
@@ -13,7 +13,7 @@ def logistic_loss(X_1, X_2):
     m = len(d_1_2)
     assert n == m, "n == m required for logistic regression loss"
 
-    Y = np.concatenate([np.ones(n), 2 * np.ones(m)])
+    Y = np.concatenate([np.ones(n), 0 * np.ones(m)])
 
     moments_1 = np.column_stack([log_w_1_1, d_1_1, log_w_2_1, d_2_1, log_w_1_1**2, log_w_2_1**2])
     moments_2 = np.column_stack([log_w_1_2, d_1_2, log_w_2_2, d_2_2, log_w_1_2**2, log_w_2_2**2])