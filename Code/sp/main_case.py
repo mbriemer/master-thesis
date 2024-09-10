0501e4df7206e6918de789b97683ff3e67a79d1d
mbriemer <82475855+mbriemer@users.noreply.github.com>
2024-09-10 14:26:49 +0200

    Start translating main_case.m (up to loss plots)

---
+++ Code/sp/main_case.py
@@ -0,0 +1,84 @@
+"""main_case.m"""
+import numpy as np
+import scipy.optimize as opt
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+from roy import royinv
+from other_discriminators import logistic_loss_2
+from NND_sp import generator_loss
+
+if __name__ == "__main__":
+
+    n = m = 300 # Real and fake sample size
+    S = 500 # Number of simulations
+    g = 30 # Number of neural nets to average over
+
+    true_theta = np.array([1.8, 2, 0.5, 0, 1, 1, 0.5, 0]) # switched rho_s and rho_t
+    lower_bounds = np.array([1, 1, -.5, -1, 0, 0, -0.99, -0.99])
+    upper_bounds = np.array([3, 3, 1.5, 1, 2, 2, 0.99, 0.99])
+
+    # Observations
+    Z_X = np.random.rand(n, 4)
+    X = royinv(Z_X, true_theta)
+    Z = np.random.rand(m, 4)
+
+    # Perturb
+    theta_initial_guess = true_theta + np.random.normal(0, 0.2, len(true_theta))
+    theta_initial_guess = np.clip(theta_initial_guess, lower_bounds, upper_bounds)
+
+    """ # Pre-estimate with logistic regression
+    AdvL = opt.minimize(lambda theta : logistic_loss_2(X, royinv(Z, theta))[0],
+                        x0 = theta_initial_guess,
+                        method='Nelder-Mead',
+                        bounds = list(zip(lower_bounds, upper_bounds)),
+                        options={'return_all' : True, 'disp' : True, 'adaptive' : True})
+    print(theta_initial_guess)
+    print(AdvL.x) """
+    
+    # Other code that I skip
+
+    # Loss plots
+
+    K = 30 # Number of grid points
+    param_names = [
+        r'$\mu_1$',
+        r'$\mu_2$',
+        r'$\gamma_1$',
+        r'$\gamma_2$',
+        r'$\sigma_1$',
+        r'$\sigma_2$',
+        r'$\rho_s$',
+        r'$\rho_t$'
+    ]
+
+
+    param_grid = np.linspace(lower_bounds, upper_bounds, K).T
+    cD_grid = np.zeros_like(param_grid)
+    NND_grid = np.zeros_like(param_grid)
+
+    for i in tqdm(range(len(true_theta))):
+        for k in tqdm(range(K)):
+            theta = true_theta.copy()
+            theta[i] = param_grid[i, k]
+            cD_grid[i, k] = logistic_loss_2(X, royinv(Z, theta))[0]
+            #NND_grid[i, k] = generator_loss(X, royinv(Z, theta), num_hidden=10, num_models=g)
+
+    fig, axs = plt.subplots(4, 2)
+    axs = axs.flatten()  # Flatten the 2D array of axes to make indexing easier
+
+    for i in range(len(true_theta)):
+        ax = axs[i]
+
+        #ax.plot(param_grid[i, :], NND_grid[i, :], linewidth=1.5, color='blue', label='$\\mathbf{M}_\\theta(\\hat{D}_\\theta)$')
+        ax.plot(param_grid[i, :], cD_grid[i, :], linewidth=1.5, color='red', label='$\\mathbf{L}_\\theta$')
+
+        ax.axvline(x=true_theta[i], color='r', linestyle='--', label=f'True {param_names[i]}')
+
+        ax.set_xlim(lower_bounds[i], upper_bounds[i])
+        ax.set_ylim(-1.4, -1.25)
+
+        ax.legend(loc='best', frameon=False)
+
+    plt.tight_layout()
+    plt.show()
--- Code/sp/other_discriminators.py
+++ Code/sp/other_discriminators.py
@@ -6,7 +6,7 @@ from sklearn.linear_model import LogisticRegression
 from roy import logroypdf, royinv
 
 def logistic_loss(X_1, X_2):
-    """Function "loss" from main_roy.m"""
+    """Functions "loss" from main_roy.m and "loss1" from main_case.m"""
     log_w_1_1, d_1_1, log_w_2_1, d_2_1 = X_1
     log_w_1_2, d_1_2, log_w_2_2, d_2_2 = X_2
     n = len(d_1_1)
@@ -29,6 +29,32 @@ def logistic_loss(X_1, X_2):
     v = np.mean(logistic.logcdf(np.dot(np.column_stack([np.ones(n), moments_1]), coefficients))) + \
         np.mean(logistic.logcdf(-np.dot(np.column_stack([np.ones(m), moments_2]), coefficients)))
     
+    return v, coefficients
+
+def logistic_loss_2(X_1, X_2):
+    """Function "loss2" from main_case.m"""
+    log_w_1_1, d_1_1, log_w_2_1, d_2_1 = X_1
+    log_w_1_2, d_1_2, log_w_2_2, d_2_2 = X_2
+    n = len(d_1_1)
+    m = len(d_1_2)
+    assert n == m, "n == m required for logistic regression loss"
+
+    Y = np.concatenate([np.ones(n), 0 * np.ones(m)])
+
+    moments_1 = np.column_stack([log_w_1_1, d_1_1, log_w_2_1, d_2_1, log_w_1_1**2, log_w_2_1**2, log_w_1_1 * log_w_2_1])
+    moments_2 = np.column_stack([log_w_1_2, d_1_2, log_w_2_2, d_2_2, log_w_1_2**2, log_w_2_2**2, log_w_1_2 * log_w_2_2])
+
+    # Fit logistic regression model
+    lr_model = LogisticRegression(fit_intercept=True)
+    lr_model.fit(np.vstack([moments_1, moments_2]), Y)
+
+    # Extract coefficients (lambda in MATLAB code)
+    coefficients = np.concatenate([lr_model.intercept_, lr_model.coef_[0]])
+
+    # Calculate loss
+    v = np.mean(logistic.logcdf(np.dot(np.column_stack([np.ones(n), moments_1]), coefficients))) + \
+        np.mean(logistic.logcdf(-np.dot(np.column_stack([np.ones(m), moments_2]), coefficients)))
+                
     return v, coefficients
 
 def OracleD(x, y, th_x, th_y):
