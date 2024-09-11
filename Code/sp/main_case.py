18d90930b8cf250e45550611b7aa0f214a1a77ba
mbriemer <82475855+mbriemer@users.noreply.github.com>
2024-09-11 12:55:07 +0200

    Extend main_case.py
    Add parallelizable estimation, experimental callback
    Comment out loss plots

--- Code/sp/main_case.py
+++ Code/sp/main_case.py
@@ -1,17 +1,50 @@
 """main_case.m"""
 import numpy as np
+from numpy.random import choice
 import scipy.optimize as opt
 import matplotlib.pyplot as plt
+import multiprocessing as mp
 from tqdm import tqdm
 
 from roy import royinv
 from other_discriminators import logistic_loss_2
 from NND_sp import generator_loss
 
+def callback(intermediate_result):
+    print(intermediate_result)
+
+def run_single_repetition(args):
+    rep, Z, X, n, m, lower_bounds, upper_bounds, theta_0, g = args
+    print(f"Starting repetition {rep} with pre-estimated guess {theta_0}")
+    #bX = np.random.choice(n, n)
+    #bU = np.random.choice(m, m)
+    #Xb = np.asarray(X).T[bX,:]
+    #Zb = np.asarray(Z)[bU,:]
+
+    Xb = choice(X[0], n), choice(X[1], n), choice(X[2], n), choice(X[3], n)
+    Zb = np.asarray([choice(Z[0], m), choice(Z[1], m), choice(Z[2], m), choice(Z[3], m)]).T
+    
+    print(f"Starting logistic in repetition {rep}")
+    AdvL = opt.minimize(lambda theta : logistic_loss_2(Xb, royinv(Zb, theta))[0],
+                        x0 = theta_0,
+                        method='Nelder-Mead',
+                        bounds = list(zip(lower_bounds, upper_bounds)),
+                        options={'return_all' : True, 'disp' : False, 'adaptive' : True})
+    
+    print(f"Starting NND in repetition {rep}")
+    AdvN = opt.minimize(lambda theta : generator_loss(Xb, royinv(Zb, theta), num_hidden=10, num_models=g),
+                        x0 = AdvL.x,
+                        method='Nelder-Mead',
+                        bounds = list(zip(lower_bounds, upper_bounds)),
+                        callback=callback,
+                        options={'return_all' : True, 'disp' : True, 'adaptive' : True, 'maxiter' : 50})
+    
+    return rep, theta_0, AdvN.x
+
 if __name__ == "__main__":
 
     n = m = 300 # Real and fake sample size
-    S = 500 # Number of simulations
+    S = 1 # Number of repetitions
     g = 30 # Number of neural nets to average over
 
     true_theta = np.array([1.8, 2, 0.5, 0, 1, 1, 0.5, 0]) # switched rho_s and rho_t
@@ -27,14 +60,15 @@ if __name__ == "__main__":
     theta_initial_guess = true_theta + np.random.normal(0, 0.2, len(true_theta))
     theta_initial_guess = np.clip(theta_initial_guess, lower_bounds, upper_bounds)
 
-    """ # Pre-estimate with logistic regression
+    # Pre-estimate with logistic regression
+    print("Pre-estimating with logistic regression")
     AdvL = opt.minimize(lambda theta : logistic_loss_2(X, royinv(Z, theta))[0],
                         x0 = theta_initial_guess,
                         method='Nelder-Mead',
                         bounds = list(zip(lower_bounds, upper_bounds)),
                         options={'return_all' : True, 'disp' : True, 'adaptive' : True})
     print(theta_initial_guess)
-    print(AdvL.x) """
+    print(AdvL.x)
     
     # Other code that I skip
 
@@ -51,6 +85,7 @@ if __name__ == "__main__":
         r'$\rho_s$',
         r'$\rho_t$'
     ]
+    """
 
 
     param_grid = np.linspace(lower_bounds, upper_bounds, K).T
@@ -82,4 +117,21 @@ if __name__ == "__main__":
         ax.legend(loc='best', frameon=False)
 
     plt.tight_layout()
-    plt.show()
+    plt.savefig('./simres/loss_plots.png') 
+    """
+    #plt.show()
+
+    # Estimation
+
+    num_processes = mp.cpu_count()
+
+    args_list = [(rep, Z, X, n, m, lower_bounds, upper_bounds, AdvL.x, g) for rep in range(S)]
+
+    results = []
+    with mp.Pool(processes=num_processes) as pool:
+        for result in pool.imap_unordered(run_single_repetition, args_list):
+            results.append(result)
+            rep, initial_value, final_value = result
+            np.savez(f'./simres/intermediate_result_{rep}.npz', 
+                     initial_value=initial_value, 
+                     final_value=final_value)
